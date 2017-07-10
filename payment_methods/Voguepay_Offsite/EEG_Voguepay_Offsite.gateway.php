a095eaf5ec7a7abb74d38c996515adc9546afd03
xyluz <xyluz@ymail.com>
2017-07-10 21:39:00 +0100

    first completion

--- payment_methods/Voguepay_Offsite/EEG_Voguepay_Offsite.gateway.php
+++ payment_methods/Voguepay_Offsite/EEG_Voguepay_Offsite.gateway.php
@@ -62,26 +62,11 @@ class EEG_Voguepay_Offsite extends EE_Offsite_Gateway{
 	public function handle_payment_update($update_info, $transaction) {
 		$payment = $transaction->last_payment();
 			
-		if(isset( $update_info[ 'resp' ] ) ){
-			if( $update_info[ 'resp' ] == 00 ){
+		if(isset( $update_info[ 'status' ] ) ){
+			if( $update_info[ 'status' ] == "Approved" ){
 				$billing_info[ 'credit_card' ] = 1;	
 				$payment->set_status( $this->_pay_model->approved_status() );
 				$payment->set_gateway_response( __( 'Payment Approved', 'event_espresso' ));
-			}else if($update_info[ 'resp' ] == 33){
-				$payment->set_status( $this->_pay_model->failed_status() );
-				$payment->set_gateway_response( __( 'Expired Card, Pick-Up', 'event_espresso' ));
-			}
-			else if($update_info[ 'resp' ] == 51){
-				$payment->set_status( $this->_pay_model->failed_status() );
-				$payment->set_gateway_response( __( 'Insufficient Funds', 'event_espresso' ));
-			}
-			else if($update_info[ 'resp' ] == 55){
-				$payment->set_status( $this->_pay_model->failed_status() );
-				$payment->set_gateway_response( __( 'Incorrect PIN', 'event_espresso' ));
-			}
-			else if($update_info[ 'resp' ] == Z6){
-				$payment->set_status( $this->_pay_model->failed_status() );
-				$payment->set_gateway_response( __( 'Incomplete Transaction', 'event_espresso' ));
 			}
 			else{
 				$payment->set_status( $this->_pay_model->failed_status());
@@ -98,8 +83,8 @@ class EEG_Voguepay_Offsite extends EE_Offsite_Gateway{
 	public function set_settings($settings_array){
 		parent::set_settings($settings_array);
 		$this->_gateway_url = $this->_debug_mode
-			? 'https://sandbox.interswitchng.com/collections/w/pay'
-			: 'https://sandbox.interswitchng.com/collections/w/pay';
+			? 'https://voguepay.com/pay/'
+			: 'https://voguepay.com/pay/';
 	}
 	
 	public function getHas($mac_key, $txn_ref, $prod_id, $pay_item_id, $amount, $return_url){
@@ -116,45 +101,34 @@ class EEG_Voguepay_Offsite extends EE_Offsite_Gateway{
 	 * @param type $cancel_url
 	 */
 	public function set_redirection_info($payment, $billing_info = array(), $return_url = NULL, $notify_url = NULL, $cancel_url = NULL) {
+
+
 			
 		global $auto_made_thing_seed;
 		
 		if( empty( $auto_made_thing_seed ) ) {
 			$auto_made_thing_seed = rand(1,1000000);
 		}
-		//$return_url = EE_INTERSWITCH_URL .DS . 'payment_methods' . DS . 'Interswitch_Offsite' . DS . 'request.php';
+			
 		
-		$cust_id = $payment->set_txn_id_chq_nmbr( $auto_made_thing_seed++ );
-	
-		$txn_ref = "fgx". rand(0,99).rand(0,99).rand(0,100).$cust_id;
-		$mac_key = 'D3D1D05AFE42AD50818167EAC73C109168A0F108F32645C8B59E897FA930DA44F9230910DAC9E20641823799A107A02068F7BC0F4CC41D2952E249552255710F';
-		
-		$hashed = $this->getHas($mac_key, $txn_ref, 1076, 101, $payment->amount()*100, $return_url);
-		
-				
-		
-		$payment->set_redirect_url('https://sandbox.interswitchng.com/collections/w/pay');
+		$payment->set_redirect_url('https://voguepay.com/pay/');
 
 		$primary_attendee = $payment->get_primary_attendee();
 		
-		/* @var $primary_attendee EE_Attendee */
-		$first_name = $primary_attendee->fname();
-		$last_name = $primary_attendee->lname();
-		$cust_full_name = $first_name . " " . $last_name;
-		
-					
+			
 		$payment->set_redirect_args( array(
-			'amount' => $payment->amount()*100,			
-			'site_redirect_url' => $return_url,
-			'product_id'=> 1076,
-			'cust_id' => 2358,
-			'cust_name' => $cust_full_name,
-			'pay_item_id' => 101,
-			'pay_item_name' => "Registration",
-			'currency' => 566,
-			'txn_ref' => $txn_ref,
-			'hash' => $hashed,
+			'total' => $payment->amount()*100,			
+			'notify_url' => $return_url,
+			'success_url' => $return_url,
+			'fail_url' => $return_url,
+			'v_merchant_id'=> 'qa331322179752',
+			'merchant_ref' => '234-567-890',
+			'developer_code' => '',
+			'store_id' => 25,
+			'memo' => "Registration",
+			
 		));
+		
 		return $payment;		
 		
 	}
